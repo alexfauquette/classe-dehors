0f620a7c92bb8a9b715353b7ea2d979884445392
Alexandre Fauquette <alex.fauquette@gmail.com>
2021-10-05 09:54:32 +0200

    Updates

---
+++ precache-manifest.bf64105a6e239003c95d8c7cc9414b84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "288ee6354ede6538bef150d57eeff714",
+    "url": "/classe-dehors/index.html"
+  },
+  {
+    "revision": "0b3dbae04d7d99d03e54",
+    "url": "/classe-dehors/static/css/2.5100ad1e.chunk.css"
+  },
+  {
+    "revision": "6863ceb9f74ec1c67792",
+    "url": "/classe-dehors/static/css/main.ce5ef230.chunk.css"
+  },
+  {
+    "revision": "0b3dbae04d7d99d03e54",
+    "url": "/classe-dehors/static/js/2.a0a28a34.chunk.js"
+  },
+  {
+    "revision": "79e99cc2fb378c8495ab5211ddfb6f87",
+    "url": "/classe-dehors/static/js/2.a0a28a34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6863ceb9f74ec1c67792",
+    "url": "/classe-dehors/static/js/main.3080a0ab.chunk.js"
+  },
+  {
+    "revision": "f402a33ab0fc3c61cc08",
+    "url": "/classe-dehors/static/js/runtime-main.90eeda6a.js"
+  }
+]);
