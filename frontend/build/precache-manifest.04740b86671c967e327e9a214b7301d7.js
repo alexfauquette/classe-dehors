d618a556adb053864a9727b3cea533bbef7f0ee1
Alexandre Fauquette <alex.fauquette@gmail.com>
2021-10-19 10:46:42 +0200

    deploy

---
+++ frontend/build/precache-manifest.04740b86671c967e327e9a214b7301d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95f094730e5507de3ee73cd03fed2af4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "569cbd345f51a5636239",
+    "url": "/static/css/main.ce5ef230.chunk.css"
+  },
+  {
+    "revision": "77e85bd5673a80bf1a0d",
+    "url": "/static/js/2.fbe1b8b5.chunk.js"
+  },
+  {
+    "revision": "cd0ca5874b2ae9c5615f4765f19eb6a3",
+    "url": "/static/js/2.fbe1b8b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "569cbd345f51a5636239",
+    "url": "/static/js/main.a2ac3262.chunk.js"
+  },
+  {
+    "revision": "e18b07de9e40e9268667",
+    "url": "/static/js/runtime-main.12aa4f03.js"
+  }
+]);
